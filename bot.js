2dd78b16322c164d7a165135a885a5e6e2a990e3
areetsheth <70079135+areetsheth@users.noreply.github.com>
2021-02-16 15:41:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,34 @@
-console.log('hello');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const stockBot = '762754228464517171';
+const prefix = '$';
+
+// Bot login
+client.login(process.env.BOT_TOKEN);
+
+// Initial connect to Discord
+client.once('ready', () => {
+    console.log('Areet stock bot is online!');
+    client.user.setActivity("ticker for link"); 
+});
+
+function send(toSend) {
+    let channel = client.channels.cache.get(stockBot);
+    channel.send(toSend);
+}
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+    try {
+        let embed = new Discord().MessageEmbed()
+        .setColor('green')
+        .setTitle('Market Watch Link')
+        .setDescription('[' + symbol + '](https://www.marketwatch.com/investing/stock/' + symbol + '?mod=quote_search)');
+
+        send(embed);
+    } catch (e) {
+        send('Might be wrong. Perhaps wrong symbol?');
+    }
+});
